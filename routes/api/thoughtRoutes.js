c515935271b85b1e4054ba8d2ac7c4a4ae582c77
Yahvannel <yahvannellopez@gmail.com>
2024-08-13 18:59:11 -0500

    added final routes to userRoutes and thoughtRoutes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -5,13 +5,22 @@ const {
   createThought,
   updateThought,
   deleteThought,
+  createReaction,
+  removeReaction
 } = require('../../controllers/thoughtController');
 
 // /api/students
 router.route('/').get(getThoughts).post(createThought);
 
 // /api/students/:studentId
-router.route('/:thoughtId').get(getSingleThought).delete(deleteThought).put(updateThought);
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
+router
+.route('/:thoughtId/reactions')
+.post(createReaction)
+
+router
+.route('/:thoughtId/reactions/:reactionId')
+.delete(removeReaction)
 
 module.exports = router;
